0f5004ad1d71a99255a304084ee319a022b40be1
soffanderson <s.btudz@gmail.com>
2023-06-20 22:51:05 +0800

    Proses menampilkan konten dari API

---
+++ src/scripts/views/templates/template-creator.js
@@ -0,0 +1,44 @@
+/* eslint-disable indent */
+import CONFIG from '../../globals/config';
+
+const createRestoDetailTemplate = (resto) => `
+      <h2 class="movie__title">${resto.title}</h2>
+      <img class="movie__poster" src="${CONFIG.BASE_IMAGE_URL + resto.poster_path}" alt="${resto.title}" />
+      <div class="movie__info">
+        <h3>Information</h3>
+        <h4>Tagline</h4>
+        <p>${resto.tagline}</p>
+        <h4>Release Date</h4>
+        <p>${resto.release_date}</p>
+        <h4>Duration</h4>
+        <p>${resto.runtime} minutes</p>
+        <h4>Rating</h4>
+        <p>${resto.vote_average}</p>
+      </div>
+      <div class="movie__overview">
+        <h3>Overview</h3>
+        <p>${resto.overview}</p>
+      </div>
+    `;
+
+const createMovieItemTemplate = (movie) => `
+      <div class="movie-item">
+        <div class="movie-item__header">
+          <img class="movie-item__header__poster" alt="${movie.title}"
+               src="${
+                 movie.backdrop_path
+                   ? CONFIG.BASE_IMAGE_URL + movie.backdrop_path
+                   : 'https://picsum.photos/id/666/800/450?grayscale'
+               }">
+          <div class="movie-item__header__rating">
+            <p>⭐️<span class="movie-item__header__rating__score">${movie.vote_average}</span></p>
+          </div>
+        </div>
+        <div class="movie-item__content">
+          <h3><a href="/#/detail/${movie.id}">${movie.title}</a></h3>
+          <p>${movie.overview}</p>
+        </div>
+      </div>
+    `;
+
+export { createMovieItemTemplate, createMovieDetailTemplate };
